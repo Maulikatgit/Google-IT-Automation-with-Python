97a1bc782455037edf977f6021500a62a095df5d
Maulik Parmar <109577079+Maulikatgit@users.noreply.github.com>
2023-03-18 21:52:14 +0530

    Course 2 Week 1

---
+++ Using Python to Interact with the Operating System/Week 1/Practical_Example.py
@@ -0,0 +1,16 @@
+import shutil
+import psutil
+
+def check_disk_usage(disk):
+    DU = shutil.disk_usage(disk)
+    free = DU.free / DU.total * 100
+    return free > 20
+
+def check_cpu_usage():
+    usage = psutil.cpu_percent(1)
+    return usage > 75
+
+if not check_disk_usage("/") or not check_cpu_usage():
+    print ("ERROR")
+else:
+    print ("Everything is OK")
